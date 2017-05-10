01467efe2d4cb1daae4dcf96886fc5ba211eb702
Dee Davis <threedaycleanse@gmail.com>
2017-05-10 11:19:04 -0400

    Sign Out Working

--- project-4-new/config/routes.rb
+++ project-4-new/config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # devise_for :users
 
 get '/users' => "users#show", as: "user"
 
 get '/admins' => "admins#show", as: "admin"
+
+# get "users/sign_up" => "users#new"
 
 resources :careers
 
