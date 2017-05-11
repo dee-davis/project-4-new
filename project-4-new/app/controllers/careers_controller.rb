77f25d167bf477e0240bd9c099c89d7ee8d100b4
Dee Davis <threedaycleanse@gmail.com>
2017-05-10 22:45:12 -0400

    Styling

--- project-4-new/app/controllers/careers_controller.rb
+++ project-4-new/app/controllers/careers_controller.rb
@@ -3,8 +3,6 @@ def index
 end
 # create code
 def create
-  # @career = Post.create (content: post_params)
-  # if @career.save
     redirect_to careers_path, notice: "You posted a Career sucessfully!"
   # else render :new
   end
@@ -16,4 +14,9 @@ def new
 #
 #   end
 end
+end
+
+def careers_path
+  params.require(:career).permit(:name, :description, :location, :title)
+
 end
